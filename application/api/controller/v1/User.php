23e7327c0dffc6a316e2746d37b66c67a096798b
akgstudio <bumaya1024@gmail.com>
2025-12-16 22:48:10 +0800

    接口

--- application/api/controller/v1/User.php
+++ application/api/controller/v1/User.php
@@ -13,7 +13,7 @@ class User extends Controller
     // 获取当前用户信息（假设当前用户ID为1，实际应根据登录状态获取）
     public function index()
     {
-        $user = UserModel::get(1);
+        $user = UserModel::get(input('id',1));
         if ($user) {
             return json(['code' => 200, 'msg' => '成功', 'data' => $user]);
         } else {
@@ -55,8 +55,39 @@ class User extends Controller
         ];
       
         $token = JWT::encode($payload, $key, 'HS256');
-        return json(['code' => 0, 'msg' => '登录成功', 'data' => [
-            'token' => $token
+        return json(['code' => 200, 'msg' => '登录成功', 'data' => [
+            'token' => $token,
+            'userid' => $info['id']
         ]]);
+        
+    }   
+
+    public function register(Request $request)
+    {
+        $data = $request->param();
+        $user= new UserModel();
+        if(empty($data['username']) || empty($data['password'])){
+            return json(['code' => 3, 'msg' => '用户名或密码不能为空']);
+        }
+        $info = $user->where('username', $data['username'])->find();
+        if ($info) {
+            return json(['code' => 1, 'msg' => '用户已存在']);
+        }
+        $ret = $user->save($data);
+        if($ret){
+        return json(['code' => 200, 'msg' => '注册成功',]);
+        }else{
+            return json(['code' => 2, 'msg' => '注册失败',]);
+        }
+        // $key = 'api';
+        // $payload = [
+        //     'iss' => 'http://rbac',
+        //     'aud' => 'http://rbac',
+        //     'iat' => time(),
+        //     'exp' => time() + 60 * 60 * 24 * 365,
+        //     'aid' => $user['id'],
+        // ];
+      
+        // $token = JWT::encode($payload, $key, 'HS256');
     }   
 }
